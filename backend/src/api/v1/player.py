a8a5513bf9449f3006f5915253bdcac99aa18974
nickatnight <kay.muneee@gmail.com>
2023-03-08 01:28:06 -0800

    chore: add headers for react-admin

--- backend/src/api/v1/player.py
+++ backend/src/api/v1/player.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, Response
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.db.session import get_session
@@ -19,11 +19,15 @@ router = APIRouter()
     tags=["players"],
 )
 async def players(
+    response: Response,
     skip: int = Query(0, ge=0),
     limit: int = Query(100, ge=1),
+    sort: str = Query(default="created_utc"),
+    order: str = Query(default="desc"),
     session: AsyncSession = Depends(get_session),
 ) -> IGetResponseBase[List[IPlayerRead]]:
     repo = PlayerRepository(db=session)
     players = await repo.all(skip=skip, limit=limit)
 
+    response.headers["x-content-range"] = f"{len(players)}/{10}"
     return IGetResponseBase[List[IPlayerRead]](data=players)
